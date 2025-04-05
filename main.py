0badb627faa9e628ad42ca4098c6423fc1ca6012
gbean4 <gbean4@byu.edu>
2025-04-04 19:47:04 -0600

    created main.py for application

---
+++ main.py
@@ -0,0 +1,10 @@
+#using this code to deploy my to do list model
+
+import pandas as pd
+import streamlit as st
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+st.title('My To Do List')
+st.subheader('This is your to do list for the day. Feel free to add new tasks and check off complted ones. Ka-ching!')
+
