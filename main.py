cfb156bde8f6fcae7f28b84430edb99cdaa85635
gbean4 <gbean4@byu.edu>
2025-04-04 20:15:26 -0600

    add status column

--- main.py
+++ main.py
@@ -6,5 +6,19 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 st.title('My To Do List')
-st.subheader('This is your to do list for the day. Feel free to add new tasks and check off complted ones. Ka-ching!')
+st.subheader('This is your to do list for the day. Feel free to add new tasks and check off completed ones. Ka-ching!')
 
+canvas = pd.read_csv('canvas.csv')
+today_date = st.date_input("Enter today's date:", value="today", format="YYYY/MM/DD")
+
+for item in canvas['summary']:
+    st.checkbox(item)
+
+# for date, index, description, item in canvas.iterrows():
+#     st.checkbox(item)
+    # task = row['summary']
+    # completed = row['Completed']
+    # if st.checkbox(task, value=completed):
+    #     canvas.at[index, 'Completed'] = True
+    # else:
+    #     canvas.at[index, 'Completed'] = False
