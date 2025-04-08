abaa0bbcadb20245aeb1850528eee92634a41bec
gbean4 <gbean4@byu.edu>
2025-04-07 20:05:24 -0600

    remove random

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import streamlit as st
 import seaborn as sns
 import matplotlib.pyplot as plt
 from datetime import datetime
-import random
 
 #date input for the user to select the date
 # st.write("Please select the date for your tasks:")
@@ -39,7 +38,7 @@ task_due_date = pd.to_datetime(task_due_date)
 # task_priority = st.selectbox("Priority", [3, 2, 1], format_func=lambda x: {3: "High", 2: "Medium", 1: "Low"}[x])
 if st.button("Add Task"):
     if task_title:
-        new_task = pd.DataFrame({'summary': [task_title], 'dtstart': [task_due_date], 'status': ['F'], 'uid': [f"event-assignment-{random.randint(1, 1000)}"]})
+        new_task = pd.DataFrame({'summary': [task_title], 'dtstart': [task_due_date], 'status': ['F']})
         st.session_state.canvas = pd.concat([canvas, new_task], ignore_index=True)
         st.success(f"Task '{task_title}' added successfully!")
         canvas = pd.concat([canvas, new_task], ignore_index=True)
